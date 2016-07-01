18c53768cfffb626177d65db78767b336b5938f8
Dian Fay <dian.m.fay@gmail.com>
2016-07-01 18:32:14 -0400

    streamlining

--- i3/restore.sh
+++ i3/restore.sh
@@ -4,8 +4,6 @@ for layout in $(ls ~/.config/i3/layouts/*.json); do
   i3-msg "workspace $(basename $layout .json); append_layout $layout"
 done
 
-(atom &)
-(urxvt &)
 (vivaldi-stable &)
 (keepassx2 &)
 (sylpheed &)
