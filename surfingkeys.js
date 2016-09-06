572890d0a1fd6489d558f2ec51fb1fa4cbafb638
Dian Fay <dian.m.fay@gmail.com>
2016-09-06 18:15:43 -0400

    more surfingkeys config

--- surfingkeys.js
+++ surfingkeys.js
@@ -9,13 +9,25 @@
 // an example to remove mapkey `Ctrl-i`
 // unmap('<Ctrl-i>');
 
-Hints.style("border: solid 3px #C38A22; color:red;");
+Hints.style('border: solid 3px #552a48; color:#efe1eb; background: initial; background-color: #552a48;');
 
+Visual.style('marks', 'background-color: #89a1e2;');
+Visual.style('cursor', 'background-color: #9065b7;');
 map('<Ctrl-[>', '<Esc>');
+
+mapkey('u', 'Scroll a page up', 'Normal.scroll("pageUp")');
+mapkey('d', 'Scroll a page down', 'Normal.scroll("pageDown")');
 
 // click `Save` button to make above settings to take effect.
 // set theme
 settings.theme = '\
+.surfingkeys_cursor { \
+    background-color: #89065b !important; \
+    color: #9065b7 !important; \
+} \
+surfingkeys_mark { \
+    background-color: #89a1e2 !important; \
+} \
 .sk_theme { \
     background: #16161f; \
     color: #efe1eb; \
