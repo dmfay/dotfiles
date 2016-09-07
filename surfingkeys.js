d129c5b9a1aaaf6941c834d8cd263107157934f2
Dian Fay <dian.m.fay@gmail.com>
2016-09-07 08:37:33 -0400

    more surfingkeys config

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,29 +1,22 @@
-// an example to create a new mapping `ctrl-y`
-// mapkey('<Ctrl-y>', 'Show me the money', function() {
-//     Front.showPopup('a well-known phrase uttered by characters in the 1996 film Jerry Maguire (Escape to close).');
-// });
-
-// an example to replace `u` with `?`, click `Default mappings` to see how `u` works.
-// map('?', 'u');
-
 // an example to remove mapkey `Ctrl-i`
 // unmap('<Ctrl-i>');
 
 Hints.style('border: solid 3px #552a48; color:#efe1eb; background: initial; background-color: #552a48;');
 
-Visual.style('marks', 'background-color: #89a1e2;');
-Visual.style('cursor', 'background-color: #9065b7;');
+Visual.style('marks', 'background-color: #97d3dc;');
+Visual.style('cursor', 'background-color: #552a48;');
+
 map('<Ctrl-[>', '<Esc>');
 
-mapkey('u', 'Scroll a page up', 'Normal.scroll("pageUp")');
-mapkey('d', 'Scroll a page down', 'Normal.scroll("pageDown")');
+map('u', 'e');  // right-handed scroll up
+map('F', 'af'); // open in new tab
+map('J', 'E');  // left tab
+map('K', 'R');  // right tab
 
-// click `Save` button to make above settings to take effect.
-// set theme
 settings.theme = '\
 .surfingkeys_cursor { \
     background-color: #89065b !important; \
-    color: #9065b7 !important; \
+    color: #552a48 !important; \
 } \
 surfingkeys_mark { \
     background-color: #89a1e2 !important; \
