433b8296a69390b153d1c147dbe7221e4bf2841f
Dian Fay <dian.m.fay@gmail.com>
2017-12-03 20:18:07 -0500

    not used

--- zsh/work.zsh
+++
@@ -1,9 +0,0 @@
-# useful aliases and configuration for specific projects
-
-alias ku='kubectl config use-context minikube'
-alias kudev='kubectl config use-context aws_assure-dev'
-
-function deploy {
-  cd ~/work/infrastructure/kubernetes-templates/assure/0
-  ./deploy.sh "$@"
-}
