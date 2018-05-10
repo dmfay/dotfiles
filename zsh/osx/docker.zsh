c2f429911debee7dd5171ba6f2a51597142b8279
Dian Fay <dian.m.fay@gmail.com>
2018-05-10 17:15:01 -0500

    fix docker-machine env call

--- zsh/osx/docker.zsh
+++ zsh/osx/docker.zsh
@@ -1,10 +1,1 @@
-async_start_worker docker_settings -n
-
-on_completion() {
-  # eval stdout of command
-  eval $3
-}
-
-async_register_callback docker_settings on_completion
-
-async_job docker_settings docker-machine env default
+eval $(docker-machine env default)
